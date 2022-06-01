cdc9e96d7b5d10fcd4511e6acb083e174904c7ce
Jacob Brook <brookjacob@gmail.com>
2022-06-01 15:34:00 -0600

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+};
